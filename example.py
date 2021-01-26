4cbbfba7c9da762a5df98c6d707b4f5e6fe73b52
HaoranZ99 <haoranz9@bu.edu>
2021-01-26 17:26:16 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 '''
 ================================
 Defining my own functions here
@@ -10,6 +12,12 @@ def add(a, b):
 def subtract(a, b):
   return a - b
 
+def numpy_add(a, b):
+  return np.add(a, b)
+
+def numpy_sub(a, b):
+  return np.substract(a, b)
+
 '''
 =====================================
 Defining my own testing fuctions here
@@ -17,9 +25,12 @@ Defining my own testing fuctions here
 '''
 def test_add():
   assert add(1, 2) == 3
-
+  assert add('Boston', 'University') == 'BostonUniversity'
+  assert numpy_add(1, 2) == 3
+  
 def test_subtract():
   assert subtract(1, 2) == -1
+  assert numpy_sub(1, 2) == - 1
  
  
 
