0c112c081cb5f35d253842c77e025a773d5cbb9e
HaoranZ99 <haoranz9@bu.edu>
2021-01-26 17:08:37 +0800

    Create example.py

---
+++ example.py
@@ -0,0 +1,26 @@
+'''
+================================
+Defining my own functions here
+================================
+'''
+
+def add(a, b):
+  return a + b
+
+def subtract(a, b):
+  return a - b
+
+'''
+=====================================
+Defining my own testing fuctions here
+=====================================
+'''
+def test_add():
+  assert add(1, 2) == 3
+
+def test_subtract():
+  assert substract(1, 2) == -1
+ 
+ 
+
+
